233e4958b3a219ffa798de6cad7d0d1eed6319bc
k2font <shoichiro.kouno@gmail.com>
2023-08-28 00:46:32 +0900

    Storybookのセットアップ

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,15 @@
 import Image from 'next/image'
+import { Header } from '../stories/Header'
 
 export default function Home() {
   return (
     <main>
+      <Header
+        user={{name: "shoichiro"}}
+        onLogin={() => {}}
+        onLogout={() => {}}
+        onCreateAccount={() => {}}
+      />
       <h1>complex-ui-playground</h1>
     </main>
   )
