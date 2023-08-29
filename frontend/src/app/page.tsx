46973f2f6835a1f3f616571cb24384067bfd3dc9
k2font <shoichiro.kouno@gmail.com>
2023-08-30 00:01:29 +0900

    レイアウトの再調整(グリッドの区分けの変更)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,11 +8,112 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <div className="preview-container">
+      <div className="preview1-container">
         <Preview />
       </div>
-      <div className="output-container"></div>
+      <div className="preview2-container">
+        <Preview />
+      </div>
+      <div className="output-container">
+        <Preview />
+      </div>
       <div className="clips-container">
+        <Clip
+          name="clip1"
+          video="/test1.mp4"
+        />
+        <Clip
+          name="clip2"
+          video="/test2.mp4"
+        />
+        <Clip
+          name="clip3"
+          video="/test3.mp4"
+        />
+        <Clip
+          name="clip4"
+          video="/test4.mp4"
+        />
+        <Clip
+          name="clip5"
+          video="/test5.mp4"
+        />
+        <Clip
+          name="clip6"
+          video="/test6.mp4"
+        />
+        <Clip
+          name="clip7"
+          video="/test7.mp4"
+        />
+        <Clip
+          name="clip8"
+          video="/test8.mp4"
+        />
+        <Clip
+          name="clip1"
+          video="/test1.mp4"
+        />
+        <Clip
+          name="clip2"
+          video="/test2.mp4"
+        />
+        <Clip
+          name="clip3"
+          video="/test3.mp4"
+        />
+        <Clip
+          name="clip4"
+          video="/test4.mp4"
+        />
+        <Clip
+          name="clip5"
+          video="/test5.mp4"
+        />
+        <Clip
+          name="clip6"
+          video="/test6.mp4"
+        />
+        <Clip
+          name="clip7"
+          video="/test7.mp4"
+        />
+        <Clip
+          name="clip8"
+          video="/test8.mp4"
+        />
+        <Clip
+          name="clip1"
+          video="/test1.mp4"
+        />
+        <Clip
+          name="clip2"
+          video="/test2.mp4"
+        />
+        <Clip
+          name="clip3"
+          video="/test3.mp4"
+        />
+        <Clip
+          name="clip4"
+          video="/test4.mp4"
+        />
+        <Clip
+          name="clip5"
+          video="/test5.mp4"
+        />
+        <Clip
+          name="clip6"
+          video="/test6.mp4"
+        />
+        <Clip
+          name="clip7"
+          video="/test7.mp4"
+        />
+        <Clip
+          name="clip8"
+          video="/test8.mp4"
+        />
         <Clip
           name="clip1"
           video="/test1.mp4"
